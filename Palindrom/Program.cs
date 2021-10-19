493e0b89496f406a47a3a94c88a8f2bd24b2ccf1
martina.smahelova <martina.smahelova@thermofisher.com>
2021-10-19 16:38:15 +0200

    Add project files.

---
+++ Palindrom/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Palindrom
+{
+	class Program
+	{
+		//V kterémkoliv jazyce vytvořte metodu, která ověří, jestli je zadaný text palindrom tj.je možné ho číst stejně od konce i od začátku.
+		//Vstupem metody bude text a výstupem boolovská hodnota.
+
+		//V kterémkoliv jazyce vytvořte metodu, která ověří, jestli je ze zadaného textu možné sestavit palindrom.
+		//Vstupem metody bude text a výstupem boolovská hodnota.Přklad: "aabbc" vratí true, protože text je možné přeskládat na "abcba".
+
+		static void Main(string[] args)
+		{
+			string word;
+
+			Console.WriteLine("Enter Word: ");
+			word = Console.ReadLine();
+
+			Console.WriteLine($"{word} is palidrome: {IsTextPalindrome(word)}");
+
+			Console.ReadKey();
+		}
+
+		public static bool IsTextPalindrome(string word)
+		{
+			string wordFromLastCharacter = "";
+
+			for (int i = 0; i < word.Length; i++)
+			{
+				wordFromLastCharacter += word[(word.Length - 1) - i].ToString();
+			}
+
+			return word.Equals(wordFromLastCharacter, StringComparison.OrdinalIgnoreCase);
+		}
+	}
+}
